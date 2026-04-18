6ca1ac71b62ee2deedff770956eaa5d48133e663
maryamessam22-pixel <maryamessam22@gmail.com>
2026-04-18 16:33:17 +0200

    feat: setup globals for arabic support and apply Noto Kufi font

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'core/constants/app_constants.dart';
 import 'core/theme/app_theme.dart';
 import 'features/shared/splash/presentation/pages/choose_role_page.dart';
+import 'core/state/app_state.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,18 +13,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: AppConstants.appName,
-      builder: (context, child) {
-        final locale = Localizations.maybeLocaleOf(context);
-        final languageCode = locale?.languageCode ?? 'en';
-        return Theme(
-          data: AppTheme.getTheme(languageCode),
-          child: child!,
+    return AnimatedBuilder(
+      animation: AppState(),
+      builder: (context, _) {
+        final isArabic = AppState().isArabic;
+        final languageCode = isArabic ? 'ar' : 'en';
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: AppConstants.appName,
+          builder: (context, child) {
+            return Directionality(
+              textDirection: isArabic ? TextDirection.rtl : TextDirection.ltr,
+              child: Theme(
+                data: AppTheme.getTheme(languageCode).copyWith(
+                  textTheme: AppTheme.getTheme(languageCode).textTheme.apply(
+                    fontFamily: isArabic ? 'Noto Kufi Arabic' : 'Roboto',
+                  ),
+                ),
+                child: child!,
+              ),
+            );
+          },
+          home: const ChooseRolePage(),
         );
       },
-      home: const ChooseRolePage(),
     );
   }
 }
